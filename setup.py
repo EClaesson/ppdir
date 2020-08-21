74d02375615548e74b3f5f0694d610924d9666ec
EClaesson <emanuel.claesson@gmail.com>
2020-08-22 01:30:03 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
-    install_requires=[],
+    install_requires=[
+        'colorama>=0.4.3'
+    ],
     entry_points={
         'console_scripts': ['ppdir=ppdir:main'],
     },
